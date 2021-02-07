f1d2299c9f6155468519734e2b6533337ebec244
Adarsh236 <Adarshverma11236@gmail.com>
2021-02-07 17:56:14 +0200

    home page

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,89 @@
+import { DataServiceService } from './../../services/data-service.service';
+import { Component, OnInit } from '@angular/core';
+import { GlobalDataSummary } from 'src/app/models/global-data';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  loading = true;
+  globalData: GlobalDataSummary[];
+  datatable = [];
+  chart = {
+    PieChart: "PieChart",
+    ColumnChart: 'ColumnChart',
+    LineChart: "LineChart",
+    height: 500,
+    options: {
+      animation: {
+        duration: 1000,
+        easing: 'out',
+      },
+      is3D: true
+    }
+  }
+
+  constructor(private dataService: DataServiceService) { }
+
+  ngOnInit(): void {
+
+    this.dataService.getGlobalData().subscribe({
+      next: (result) => {
+        console.log(result);
+        this.globalData = result;
+        result.forEach(cs => {
+          if (!Number.isNaN(cs.confirmed)) {
+            this.totalActive += cs.active
+            this.totalConfirmed += cs.confirmed
+            this.totalDeaths += cs.deaths
+            this.totalRecovered += cs.active
+          }
+        })
+        this.initChart('c');
+      },
+      complete: () => {
+        this.loading = false;
+      }
+    })
+
+  }
+
+  updateChart(input: HTMLInputElement) {
+    console.log(input.value);
+    this.initChart(input.value)
+  }
+
+  initChart(caseType: string) {
+
+    this.datatable = [];
+    // this.datatable.push(["Country", "Cases"])
+
+    this.globalData.forEach(cs => {
+      let value: number;
+      if (caseType == 'c' && cs.confirmed > 2000)
+        value = cs.confirmed
+
+      if (caseType == 'a' && cs.active > 2000)
+        value = cs.active
+      if (caseType == 'd' && cs.deaths > 1000)
+
+        value = cs.deaths
+
+      if (caseType == 'r' && cs.recovered > 2000)
+        value = cs.recovered
+
+      this.datatable.push([
+        cs.country, value
+      ])
+    })
+    console.log(this.datatable);
+  }
+
+}
